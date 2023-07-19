36fbea44a30789b85412b758a7d78f0bdfc75ee3
Narenselvam <narenkishore75@gmail.com>
2023-07-19 22:20:25 +0530

    RPS console

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
 console.log("Helloe");
+
+const option=['rock','paper','scissor'];
+
+function getComputerChoice(){
+    computerSelection=option[(Math.floor(Math.random() * option.length))];
+    return computerSelection;
+
+}
+computerSelection=getComputerChoice();
+playerSelection='paper';
+
+function playRound(playerSelection,computerSelection){
+    if((playerSelection==='rock'&& computerSelection==='paper')||(playerSelection==='scissor'&&computerSelection==='rock')
+    ||(playerSelection==='paper'&&computerSelection==='scissor')){
+        return "You Lose!"
+    }
+    else if(playerSelection===computerSelection){
+        return "Its a tie"
+    }
+    else{
+        return "You Win!!"
+    }
+
+}
+
+console.log(getComputerChoice());
+console.log(playRound(playerSelection,computerSelection));
