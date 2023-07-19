34f91ee7552217adf3a20d6585efa5d0137792a4
Narenselvam <narenkishore75@gmail.com>
2023-07-19 23:05:52 +0530

    win or loose +counter

--- script.js
+++ script.js
@@ -3,26 +3,51 @@ console.log("Helloe");
 const option=['rock','paper','scissor'];
 
 function getComputerChoice(){
-    computerSelection=option[(Math.floor(Math.random() * option.length))];
+    let computerSelection=option[(Math.floor(Math.random() * option.length))];
     return computerSelection;
 
 }
 computerSelection=getComputerChoice();
-const playerSelection= prompt("Enter choice");
+const no= parseInt(prompt("Enter no Rounds"));
 
 function playRound(playerSelection,computerSelection){
     if((playerSelection==='rock'&& computerSelection==='paper')||(playerSelection==='scissor'&&computerSelection==='rock')
     ||(playerSelection==='paper'&&computerSelection==='scissor')){
-        return "You Lose!"
+        // let lose = 1;
+        // lose+=1;
+        return "You Lose"
     }
     else if(playerSelection===computerSelection){
-        return "Its a tie"
+        return "<h1>It's a tie.</h1>";
     }
     else{
-        return "You Win!!"
+        return 'You win!!'
     }
 
 }
 
-console.log(getComputerChoice());
-console.log(playRound(playerSelection,computerSelection));
+function game(no){
+    let win =0;
+    let lose= 0;
+    for (let i=0;i<no;i++){
+    const playerSelection= prompt("Enter choice");
+    console.log(getComputerChoice());
+    console.log(playRound(playerSelection,computerSelection));
+    let tempW =playRound(playerSelection,computerSelection);
+    if(tempW==='You Lose'){
+        lose+=1;
+    }
+    else{
+        win+=1;
+    }
+}if (win>lose){
+    console.log("YOU WON!!!");
+    console.log(win);
+}
+else{
+    console.log("YOU LOSE");
+    console.log(lose);
+}
+}
+
+game(no);
