f6224a66e067c280899cb125158161dfc20ec13b
Narenselvam <narenkishore75@gmail.com>
2023-07-20 08:52:09 +0530

    score card fix

--- script.js
+++ script.js
@@ -1,53 +1,50 @@
-console.log("Helloe");
+console.log("Hello");
 
-const option=['rock','paper','scissor'];
+const option = ['rock', 'paper', 'scissor'];
 
-function getComputerChoice(){
-    let computerSelection=option[(Math.floor(Math.random() * option.length))];
-    return computerSelection;
-
-}
-computerSelection=getComputerChoice();
-const no= parseInt(prompt("Enter no Rounds"));
-
-function playRound(playerSelection,computerSelection){
-    if((playerSelection==='rock'&& computerSelection==='paper')||(playerSelection==='scissor'&&computerSelection==='rock')
-    ||(playerSelection==='paper'&&computerSelection==='scissor')){
-        // let lose = 1;
-        // lose+=1;
-        return "You Lose"
-    }
-    else if(playerSelection===computerSelection){
-        return "<h1>It's a tie.</h1>";
-    }
-    else{
-        return 'You win!!'
-    }
-
+function getComputerChoice() {
+  let computerSelection = option[Math.floor(Math.random() * option.length)];
+  return computerSelection;
 }
 
-function game(no){
-    let win =0;
-    let lose= 0;
-    for (let i=0;i<no;i++){
-    const playerSelection= prompt("Enter choice");
-    console.log(getComputerChoice());
-    console.log(playRound(playerSelection,computerSelection));
-    let tempW =playRound(playerSelection,computerSelection);
-    if(tempW==='You Lose'){
-        lose+=1;
+const no = 5;
+
+function playRound(playerSelection, computerSelection) {
+  if ((playerSelection === 'rock' && computerSelection === 'paper') ||
+    (playerSelection === 'scissor' && computerSelection === 'rock') ||
+    (playerSelection === 'paper' && computerSelection === 'scissor')) {
+    return "You Lose";
+  } else if (playerSelection === computerSelection) {
+    return "It's a tie";
+  } else {
+    return 'You win!!';
+  }
+}
+
+function game(no) {
+  let win = 0;
+  let lose = 0;
+  for (let i = 0; i < no; i++) {
+    const playerSelection = prompt("Enter choice");
+    computerSelection = getComputerChoice();
+    console.log(computerSelection);
+    console.log(playRound(playerSelection, computerSelection));
+    let tempW = playRound(playerSelection, computerSelection);
+    if (tempW === "You Lose") {
+      lose += 1;
+    } else {
+      win += 1;
     }
-    else{
-        win+=1;
-    }
-}if (win>lose){
+  }
+  if (win > lose) {
     console.log("YOU WON!!!");
-    console.log(win);
-}
-else{
+    console.log("Your score", win);
+    console.log("computer  score",lose);
+  } else {
     console.log("YOU LOSE");
-    console.log(lose);
-}
+    console.log("Your score", win);
+    console.log("Computer score",lose);
+  }
 }
 
 game(no);
